a20c3bc7692c2568143a0a95a3076065c3507b51
AmrDeveloper <amr96@programmer.net>
2020-11-25 15:13:45 +0200

    Optimize SelectExecutor Code

--- src/backend/interpreter/executors/SelectExecutor.java
+++ src/backend/interpreter/executors/SelectExecutor.java
@@ -3,11 +3,18 @@ package backend.interpreter.executors;
 import backend.interpreter.Executor;
 import intermediate.ICodeNode;
 
+import java.util.HashMap;
 import java.util.List;
 
 import static intermediate.icodeimpl.ICodeKeyImpl.VALUE;
 
-public class SelectExecutor extends StatementExecutor{
+public class SelectExecutor extends StatementExecutor {
+
+    // Jump table cache: entry key is a SELECT node,
+    //                   entry value is the jump table.
+    // Jump table: entry key is a selection value,
+    //             entry value is the branch statement.
+    private static final HashMap<ICodeNode, HashMap<Object, ICodeNode>> jumpCache = new HashMap<>();
 
     public SelectExecutor(Executor executor) {
         super(executor);
@@ -15,6 +22,15 @@ public class SelectExecutor extends StatementExecutor{
 
     @Override
     public Object execute(ICodeNode node) {
+
+        // Is there already an entry for this SELECT node in the jump table cache ?
+        // If not create a new jump table entry
+        HashMap<Object, ICodeNode> jumpTable = jumpCache.get(node);
+        if(jumpTable == null) {
+            jumpTable = createJumpTable(node);
+            jumpCache.put(node, jumpTable);
+        }
+
         // Get the SELECT node's children
         List<ICodeNode> selectChildren = node.getChildren();
         ICodeNode exprNode = selectChildren.get(0);
@@ -23,63 +39,35 @@ public class SelectExecutor extends StatementExecutor{
         ExpressionExecutor expressionExecutor = new ExpressionExecutor(this);
         Object selectValue = expressionExecutor.execute(exprNode);
 
-        // Attempt to select a SELECT_BRANCH
-        ICodeNode selectBranchNode = searchBranches(selectValue, selectChildren);
-
-        // if there was a selection, execute the SELECT_BRANCH statement
-        if(selectBranchNode != null) {
-            ICodeNode stmtNode = selectBranchNode.getChildren().get(1);
+        // If there is a selection, execute the SELECT_BRANCH's statement
+        ICodeNode statementNode = jumpTable.get(selectValue);
+        if(statementNode != null) {
             StatementExecutor statementExecutor = new StatementExecutor(this);
-            statementExecutor.execute(stmtNode);
+            statementExecutor.execute(statementNode);
         }
 
-        // count the SELECT statement itself
         ++executionCount;
         return null;
     }
 
-    private ICodeNode searchBranches(Object selectValue, List<ICodeNode> selectChildren) {
-        // Loop over the SELECT_BRANCH\s to find a match
+    private HashMap<Object, ICodeNode> createJumpTable(ICodeNode node) {
+        HashMap<Object, ICodeNode> jumpTable = new HashMap<>();
+
+        //Loop over children that are SELECT_BRANCH nodes
+        List<ICodeNode> selectChildren = node.getChildren();
         for(int i = 1 ; i < selectChildren.size() ; ++i) {
             ICodeNode branchNode = selectChildren.get(i);
+            ICodeNode constantsNode = branchNode.getChildren().get(0);
+            ICodeNode statementNode = branchNode.getChildren().get(1);
 
-            if(searchConstants(selectValue, branchNode)) {
-                return branchNode;
-            }
-        }
-
-        return null;
-    }
-
-    private boolean searchConstants(Object selectValue, ICodeNode branchNode) {
-        // Are the values integer or string
-        boolean integerMode = selectValue instanceof Integer;
-
-        // Get the list of SELECT_CONSTANTS values
-        ICodeNode constantsNode = branchNode.getChildren().get(0);
-        List<ICodeNode> constantsList = constantsNode.getChildren();
-
-        // Search the list of constants
-        if(integerMode) {
+            // Loop over the constants children of the branch's CONSTANTS_NODE
+            List<ICodeNode> constantsList = constantsNode.getChildren();
             for(ICodeNode constantNode : constantsList) {
-                int constant = (Integer) constantNode.getAttribute(VALUE);
-
-                if(((Integer) selectValue) == constant) {
-                    return true;
-                }
+                // Create a jump table entry
+                Object value = constantNode.getAttribute(VALUE);
+                jumpTable.put(value, statementNode);
             }
         }
-        else {
-            for(ICodeNode constantNode : constantsList) {
-                String constant = (String) constantNode.getAttribute(VALUE);
-
-                if(selectValue.equals(constant)) {
-                    return true;
-                }
-            }
-        }
-
-        // no match
-        return false;
+        return jumpTable;
     }
 }
